043ea9e3aca93339760d6a0feddd5fae20d61af3
DatNguyen <vovanguyen11@gmail.com>
2020-07-21 00:28:08 +0300

    remove unnecessary network service methods

--- src/services/network.service.ts
+++ src/services/network.service.ts
@@ -9,29 +9,6 @@ class NetworkService {
     });
   }
 
-  private static async refreshTokenRequest() {
-    const response = await NetworkService.api.get("/user/12345", {
-      baseURL: "https://some-domain.com/auth/refresh_token/"
-    });
-    return response?.data?.access_token;
-  }
-
-  private static async prolongSession(): Promise<void> {
-    const AUTH_TOKEN = NetworkService.refreshTokenRequest();
-    if (AUTH_TOKEN) {
-      axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
-    }
-  }
-
-  private static async resendRequest(callback: () => Promise<any>) {
-    const { data, status } = await callback();
-
-    if (status === 200) {
-      return data;
-    }
-    return null;
-  }
-
   private static async requestHandler(callback: () => Promise<any>) {
     const { data, status } = await callback();
 
@@ -39,8 +16,6 @@ class NetworkService {
       case 200:
         return data;
       case 401:
-        await NetworkService.prolongSession();
-        return NetworkService.resendRequest(callback);
       case 403:
       case 500:
         return null;
@@ -55,43 +30,11 @@ class NetworkService {
     const result = await NetworkService.requestHandler(request);
     return result;
   }
-
-  public static async post(
-    url: string,
-    data: any,
-    options?: AxiosRequestConfig
-  ): Promise<any> {
-    const request = () => NetworkService.api.post(url, data, options);
-    const result = await NetworkService.requestHandler(request);
-    return result;
-  }
-
-  public static async put(
-    url: string,
-    data: any,
-    options?: AxiosRequestConfig
-  ): Promise<any> {
-    const request = () => NetworkService.api.put(url, data, options);
-    const result = await NetworkService.requestHandler(request);
-    return result;
-  }
-
-  public static async delete(
-    url: string,
-    options?: AxiosRequestConfig
-  ): Promise<any> {
-    const request = () => NetworkService.api.delete(url, options);
-    const result = await NetworkService.requestHandler(request);
-    return result;
-  }
 }
 
 NetworkService.init();
 
 const GET = NetworkService.get;
-const POST = NetworkService.post;
-const PUT = NetworkService.put;
-const DELETE = NetworkService.delete;
 
-export { GET, POST, PUT, DELETE };
+export { GET };
 export default NetworkService;
